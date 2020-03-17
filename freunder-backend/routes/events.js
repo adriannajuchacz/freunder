495c8b9349b3ed955870df956dd5bddd3c403434
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-03-17 12:28:22 +0100

    added update event

--- freunder-backend/routes/events.js
+++ freunder-backend/routes/events.js
@@ -15,23 +15,21 @@ router.get("/", (req, res) => {
 });
 
 // get event with id
-router.get('/:id', (req, res) => {
-  Event
-    .findOne({ _id: req.params.id })
-    .then(event => {
-      if (!event) {
-        res.status(404).send({
-          success: 'false',
-          message: 'No event found',
-        })
-      } else {
-        res.status(200).send({
-        success: 'true',
-        message: 'Event retrieved successfully',
+router.get("/:id", (req, res) => {
+  Event.findOne({ _id: req.params.id }).then(event => {
+    if (!event) {
+      res.status(404).send({
+        success: "false",
+        message: "No event found"
+      });
+    } else {
+      res.status(200).send({
+        success: "true",
+        message: "Event retrieved successfully",
         event: event
-      })
-      }
-    });
+      });
+    }
+  });
 });
 
 // CREATE EVENT
@@ -42,16 +40,14 @@ router.post("/", (req, res) => {
   if (!name || !link || !location || !user_id) {
     errors.push({ msg: "Please enter all fields" });
   }
-  if (date && !(date instanceof Date)) {
-    errors.push({ msg: "Date in wrong format" });
-  }
   User.findOne({ _id: user_id }).then(user => {
     if (!user) {
       errors.push({ msg: "User not found" });
     }
-    // TODO: DATE FORMAT
     if (!date) {
       date = Date.now();
+    } else {
+      date = Date.parse(date);
     }
     if (errors.length > 0) {
       res.status(400).send({
@@ -85,4 +81,58 @@ router.post("/", (req, res) => {
   });
 });
 
+// update event
+router.put("/:id", (req, res) => {
+  let { name, link, location, date, user_id } = req.body;
+  let errors = [];
+  //TODO: validation
+  if (!name || !link || !location || !user_id) {
+    errors.push({ msg: "Please enter all fields" });
+  }
+  Event.findOne({ _id: req.params.id }).then(event => {
+    if (!event) {
+      res.status(404).send({
+        success: "false",
+        message: "No event found"
+      });
+    }
+    let { name, link, location, date } = req.body;
+    let errors = [];
+    //TODO: validation
+    if (!name || !link || !location) {
+      errors.push({ msg: "Please enter all fields" });
+    }
+
+    if (!date) {
+      date = Date.now();
+    } else {
+      date = Date.parse(date);
+    }
+    if (errors.length > 0) {
+      res.status(400).send({
+        success: "false",
+        messages: errors,
+        event_create_data: req.body
+      });
+      return;
+    }
+    event = new Event({
+      name,
+      link,
+      location,
+      date
+    });
+    // SAVE EVENT
+    event
+      .save()
+      .then(event => {
+        res.status(200).send({
+          success: "true",
+          message: "Event updated successfully",
+          event: event
+        });
+      })
+      .catch(err => console.log(err));
+  });
+});
 module.exports = router;
