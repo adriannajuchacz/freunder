6604993f854de5138f91117f98f92abcbb5f68cb
Adrianna <adrianna.juchacz.biz@gmail.com>
2020-03-20 07:28:14 +0100

    added axios

--- freunder-frontend/src/actions/authActions.js
+++ freunder-frontend/src/actions/authActions.js
@@ -1,15 +1,14 @@
-import { axios } from "axios";
-import { LOGIN_SUCCESS } from "./types";
+import axios from "axios";
 
 // Login User
 export const login = () => dispatch => {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email: "jan@gmail.com", password: "password" })
-    };
-    fetch("https://freunder.uber.space/login", requestOptions)
-      .then(response => response.json())
-      .then(data => this.setState({ postId: data.id }));
-  };
-
+  const body = { email: "jan@gmail.com", password: "password" };
+  axios
+    .post("https://freunder.uber.space/login", body)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(response => {
+      console.log(response);
+    });
+};
