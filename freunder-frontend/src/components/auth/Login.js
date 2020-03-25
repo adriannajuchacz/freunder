201567b8f172f2348ac3f1ff1f26e0ba314a4759
Adrianna <adrianna.juchacz.biz@gmail.com>
2020-03-25 13:13:54 +0100

    added login & register forms (responsive)

--- freunder-frontend/src/components/auth/Login.js
+++ freunder-frontend/src/components/auth/Login.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import { connect } from "react-redux";
 import { login } from "../../actions/authActions";
 
@@ -8,14 +11,41 @@ export class Login extends Component {
   render() {
     return (
       <div>
-        <h1>LOGIN.JS</h1>
-        <form className="sth" noValidate autoComplete="off">
-          <TextField id="standard-basic" label="Standard" />
-          <TextField id="standard-basic" label="Standard" />
-          <Button variant="contained" onClick={login()}>
-            Login
-          </Button>
-        </form>
+        <h3>Log into the app with your email & password:</h3>
+        <Grid container justify="center" alignItems="center">
+          <Grid item md={6} xs={10}>
+            <Box borderColor="primary.main" borderRadius={16} p={4} border={1}>
+              <form>
+                <FormControl fullWidth>
+                  <TextField
+                    id="standard-basic"
+                    margin="normal"
+                    label="email"
+                  />
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    margin="normal"
+                    type="password"
+                    label="password"
+                  />
+                  <br />
+                </FormControl>
+                <Grid container justify="flex-end">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    margin="normal"
+                    onClick={login()}
+                  >
+                    Log in
+                  </Button>
+                </Grid>
+              </form>
+            </Box>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- freunder-frontend/src/components/auth/Register.js
+++ freunder-frontend/src/components/auth/Register.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { login } from "../../actions/authActions";
 
 export class Register extends Component {
-    render() {
-        return (
-            <div>
-                <h1>REGISTER.JS</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Grid container justify="center" alignItems="center">
+          <Grid item md={6} xs={10}>
+            <Box borderColor="primary.main" borderRadius={16} p={4} border={1}>
+              <form>
+                <FormControl fullWidth>
+                  <TextField
+                    id="standard-basic"
+                    margin="normal"
+                    label="email"
+                  />
+                  <br />
+                  <TextField id="standard-basic" margin="normal" label="name" />
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    margin="normal"
+                    type="password"
+                    label="password"
+                  />
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    margin="normal"
+                    type="password"
+                    label="re-enter password"
+                  />
+                  <br />
+                </FormControl>
+                <Grid container justify="flex-end">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    margin="normal"
+                    onClick={login()}
+                  >
+                    Register
+                  </Button>
+                </Grid>
+              </form>
+            </Box>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default Register
+export default Register;
